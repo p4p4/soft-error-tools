e9d129b72cd28d8fa12a6ff87b2c7acfc930e2d8
Patrick Klampfl <patrick.klampfl@student.tugraz.at>
2015-08-26 17:39:37 +0200

    add benchmark results, add benchmarks, adapt benchmark script

--- ImmortalTool/benchmarks/run_benchmarks.py
+++ ImmortalTool/benchmarks/run_benchmarks.py
@@ -1,32 +1,54 @@
 #!/usr/bin/python
 import subprocess
 import sys
+#############################################################################################
+##                                                                                         ##
+##   This script takes a list of circuits (without protection circuit)                     ##
+##   and converts them using AddParityTool so that they have an alarm signal.              ##
+##   The converted circuits are then used as input for the ImmortalTool, which             ##
+##   tries to search for vulnerable latches.                                               ##
+##                                                                                         ##
+############################################################################################# 
 
-immortal_binary = "./immortal-bin"
-immortal_backend = "-b stla -m 0"
+# select tool + mode
+IMMORTAL_BIN = "./immortal-bin"
+BACKEND_MODE = "-b sim -m 0"
 
+NUM_RAND_TC = "3"
+RAND_TC_LEN = "15"
+
+# set benchmarks
+BENCHMARK_DIR = "../../benchmark_files/"        # benchmarking circuits without protection circuits are in here
+#BENCHMARKS_LIST = "basic_benchmarks.txt"     # list of circuit-filenames located within the BENCHMARK_DIR
+BENCHMARKS_LIST = "standard_benchmarks.txt"
+#BENCHMARKS_LIST = "medium_benchmarks.txt"
+
+# convert input circuits with addParityTool (protects the circuit with additional latches, adds an alarm-output)
 add_parity_binary = "./../../AddParityTool/addParityTool"
-benchmark_directory = "/home/pklampfl/soft-error-analysis/AddParityTool/benchmarks/"
-list_of_benchmarks = "basic_benchmarks.txt"
 percentage_to_protect = "50"
 avg_latches = "2"
 
-with open(list_of_benchmarks) as f:
+TMP_DIR = "tmp/"
+
+# for each benchmark
+with open(BENCHMARKS_LIST) as f:
     for aiger_path in f:
         aiger_path = aiger_path.rstrip()
         print "\n================================================================================"
-        out_file = "tmp/protected_"+aiger_path.replace("/","_")
-        cmd_add_parity = add_parity_binary + " " + benchmark_directory + aiger_path + " " + percentage_to_protect + " " + avg_latches + " " + out_file
+        out_file = TMP_DIR + "protected_"+aiger_path.replace("/","_")
+        cmd_add_parity = add_parity_binary + " " + BENCHMARK_DIR + aiger_path + " " + percentage_to_protect + " " + avg_latches + " " + out_file
 
+	# convert benchmark using addParityTool
         return_code = subprocess.call(cmd_add_parity, shell=True, stdout=subprocess.PIPE)
         if return_code != 0:
             print "Error calling the addParityTool. CMD: " + cmd_add_parity
             sys.exit(0)
         sys.stdout.flush()
 
-        cmd_immortal = immortal_binary + " -i " + out_file + " -tcr 3 15 " + immortal_backend +" --print=L"
-        p = subprocess.Popen(cmd_immortal, shell=True, stderr=subprocess.PIPE) # run
-        while True: # print results
+	# execute converted benchmark with random inputs.
+        cmd_immortal = IMMORTAL_BIN + " -i " + out_file + " -tcr 3 15 " + BACKEND_MODE +" --print=L"
+        p = subprocess.Popen(cmd_immortal, shell=True, stderr=subprocess.PIPE) 
+        while True: # print output of tool
             out = p.stderr.read(1)
             if out == '' and p.poll() != None:
                 break
