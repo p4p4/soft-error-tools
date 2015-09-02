23909c17dfa60c8b94381593b82d397ea05e886e
Patrick Klampfl <patrick.klampfl@student.tugraz.at>
2015-09-02 15:15:19 +0200

    remove unused stuff

--- ImmortalTool/ext_tools/not_(yet)_used/bloqqer/blqrcfg.c
+++
@@ -1,5 +0,0 @@
-#include "blqrcfg.h"
-
-const char * blqr_id () { return BLQR_ID; }
-const char * blqr_version () { return BLQR_VERSION; }
-const char * blqr_cflags () { return BLQR_CFLAGS; }
--- ImmortalTool/ext_tools/not_(yet)_used/bloqqer/blqrcfg.h
+++
@@ -1,6 +0,0 @@
-#ifndef blqrcfg_h_INCLUDED
-#define blqrcfg_h_INCLUDED
-#define BLQR_ID ""
-#define BLQR_VERSION "034"
-#define BLQR_CFLAGS "-Wall -DNLOG -DCOMP -DNDEBUG -O3"
-#endif
