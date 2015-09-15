9795a168d2bb13e2a359ee4afdbe29a57e5ec791
Patrick Klampfl <patrick.klampfl@student.tugraz.at>
2015-09-15 11:36:51 +0200

    cleanup and document BackEnd class

--- ImmortalTool/src/BackEnd.h
+++ ImmortalTool/src/BackEnd.h
@@ -16,9 +16,6 @@
 // License along with this library; if not, see
 // <http://www.gnu.org/licenses/>.
 //
-// For more information about this software see
-//   <http://www.iaik.tugraz.at/content/research/design_verification/demiurge/>
-// or email the authors directly.
 //
 // ----------------------------------------------------------------------------
 
@@ -39,13 +36,10 @@ struct aiger;
 /// @brief An interface for the back-ends.
 ///
 /// This class provides an interface for all back-ends. Every back-end implements a different
-/// synthesis algorithm. This class is abstract, i.e., objects of this class cannot be
-/// instantiated. Instantiate one of the derived classes instead.
+/// Soft-Error-Analysis algorithm. This class is abstract, i.e., objects of this class cannot
+/// be instantiated. Instantiate one of the derived classes instead.
 ///
-/// Many back-ends can be parameterized with a circuit extraction engine that extracts a
-/// circuit from the winning region once the winning region has been computed.
-///
-/// @author Robert Koenighofer (robert.koenighofer@iaik.tugraz.at)
+/// @author Patrick Klampfl
 /// @version 1.2.0
 class BackEnd
 {
@@ -112,11 +106,31 @@ public:
 	    }
 	};
 protected:
+// -------------------------------------------------------------------------------------------
+///
+/// @brief the circuit to analyze
 	aiger* circuit_;
+
+// -------------------------------------------------------------------------------------------
+///
+/// @brief the mode (if there are more than one version and/or optimizations to enable)
 	int mode_;
+
+// -------------------------------------------------------------------------------------------
+///
+/// @brief the list of vulnerabilities found so for
 	set<unsigned> vulnerable_elements_;
+
+// -------------------------------------------------------------------------------------------
+///
+/// @brief the current Test-Case
 	TestCase &current_TC_;
 	TestCase empty_; // DUMMY TC
+
+// -------------------------------------------------------------------------------------------
+///
+/// @brief the number of error-latches. Error latches are additional latches used for the
+/// protection circuit.
 	unsigned num_err_latches_;
 
 private:
