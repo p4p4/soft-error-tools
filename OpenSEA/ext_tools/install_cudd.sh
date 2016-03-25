51b7ea25c225775a39743c8fd73b3837634f1837
Patrick Klampfl <patrick@bulme.eu>
2016-03-25 16:26:31 +0100

    update to cudd 3.0.0

--- OpenSEA/ext_tools/install_cudd.sh
+++ OpenSEA/ext_tools/install_cudd.sh
@@ -8,26 +8,23 @@ fi
 
 echo "Installing the CUDD BDD library ..."
 
-CUDD="cudd-2.5.1"
-CUDD_ACHRIVE="$CUDD.tar.gz"
+CUDD="cudd-3.0.0"
+CUDD_ARCHIVE="$CUDD.tar.gz"
 
 
 
-if [ ! -e "$CUDD_ACHRIVE" ];
+if [ ! -e "$CUDD_ARCHIVE" ];
 then
   echo " Downloading the CUDD BDD library ..."
-  wget ftp://vlsi.colorado.edu/pub/$CUDD_ACHRIVE
+  wget ftp://vlsi.colorado.edu/pub/$CUDD_ARCHIVE
 fi
 
 echo " Unpacking the CUDD BDD library ..."
 rm -rf $IMMORTALTP/cudd
-tar -xzf $CUDD_ACHRIVE -C $IMMORTALTP
+tar -xzf $CUDD_ARCHIVE -C $IMMORTALTP
 mv $IMMORTALTP/$CUDD $IMMORTALTP/cudd
 
 echo " Compiling the CUDD BDD library ..."
 cd $IMMORTALTP/cudd
-make build
-make cudd
-make obj
-make objlib
-
+./configure --enable-obj
+make
