0c2199ebd0fc0492e541c277c3771a5eea7c3717
Patrick Klampfl <patrick.klampfl@iaik.tugraz.at>
2017-01-09 16:48:27 +0100

    update ABC version

--- OpenSEA/ext_tools/not_(yet)_used/install_abc.sh
+++ OpenSEA/ext_tools/not_(yet)_used/install_abc.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-
-if [ "$DEMIURGETP" = "" ]
+# dependencies:
+# sudo apt-get install libreadline-dev mercurial
+if [ "$IMMORTALTP" = "" ]
 then
-   echo "The Environment variable DEMIURGETP is undefined."
+   echo "The Environment variable IMMORTALTP is undefined."
    exit 1
 fi
 
 echo "Installing ABC ..."
 
-cd $DEMIURGETP
+cd $IMMORTALTP
 rm -rf abc
 mkdir abc
 cd abc
 echo " Downloading ABC from GIT repository ..."
 # -r d3db71b fixes the version to the one commited on 2014-05-19
-hg clone -r d3db71b https://bitbucket.org/alanmi/abc
+hg clone https://bitbucket.org/alanmi/abc
 cd abc
 echo " Compiling ABC ..."
 make
